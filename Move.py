6b741dfdef559387f8954a4ad79a2934261595e1
Connor Fan <ConnortheFan@gmail.com>
2024-09-17 01:14:00 -0700

    Made Move class and undo/redo functions

--- Move.py
+++ Move.py
@@ -1,10 +1,42 @@
+from typing import List
+from Cell import Cell
+
+
 class Move:
     """
     A class used to represent a single move in Sudoku
     
-    Meant to be recorded for undo/redo and steps
+    Also functions as a doubly linked list
+        
+    A Move will have each change as a tuple in format (Cell, change type, number)
+    
+    Cell represent which cell is being changed
+    
+    Change type represents what kind of change is made like "fill", "addCandidate", "removeCandidate"
+    
+    Number represents which number is being changed
     """
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, prevMove = None) -> None:
+        self.changes = []
+        self.prev: Move = prevMove
+        self.next = None
+        if prevMove != None:
+            prevMove.next = self
     
+    def addChange(self, cell: Cell, changeType: str, number) -> None:
+        """Add change to the Move
+
+        Args:
+            cell (Cell): Cell being affected
+            changeType (str): "fill", "removeCandidate", "addCandidate", "empty", "clear"
+            number (int or List[int]): number(s) being affected. If multiple numbers given, use format [prev, new]
+        """
+        self.changes.append((cell, changeType, number))
+        
+    def show(self):
+        """Prints move
+        """
+        
+        for change in self.changes:
+            print(f"({change[0].row}, {change[0].col}): {change[1]} {change[2]}")
--- Sudoku.py
+++ Sudoku.py
@@ -11,7 +11,8 @@ class Sudoku:
     """
     
     board: List[List[Cell]]
-    missing: List[int]
+    startMove: Move
+    currMove: Move
     
     
     def __init__(self, filename: str = "") -> None:
@@ -22,7 +23,10 @@ class Sudoku:
         Args:
             filename (str, optional): filename of Sudoku board to create. Defaults to None.
         """
+        
         self.board = []
+        self.startMove = Move()
+        self.currMove = self.startMove
         if filename == "":
             for row in range(1,10):
                 self.board.append([])
@@ -43,16 +47,24 @@ class Sudoku:
         return self.board[row-1][col-1]
     
     def fill(self, row: int, col: int, number: int) -> None:
-        """Fills Cell with number
+        """Fills Cell with number and makes Move
 
         Args:
             row (int): row
             col (int): column
             number (int): number
         """
+        
         cell: Cell = self.getCell(row,col)
         if not cell.fixed:
+            move = Move(self.currMove)
+            move.addChange(cell, "fill", [cell.number, number])
             cell.fill(number)
+            for related in self.getRelatedUnfilled(row, col):
+                if number in related.candidates:
+                    related.removeCandidate(number)
+                    move.addChange(related, "removeCandidate", number)
+            self.currMove = move
     
     def empty(self, row: int, col: int) -> None:
         """Empty Cell by removing filled number
@@ -63,7 +75,10 @@ class Sudoku:
         """
         cell: Cell = self.getCell(row,col)
         if not cell.fixed and cell.filled:
+            move = Move(self.currMove)
+            move.addChange(cell, "empty", [cell.number, 0])
             cell.empty()
+            self.currMove = move
     
     def clear(self, row: int, col: int) -> None:
         """Clear all data in a Cell unless its fixed
@@ -74,7 +89,10 @@ class Sudoku:
         """
         cell: Cell = self.getCell(row,col)
         if not cell.fixed:
+            move = Move(self.currMove)
+            move.addChange(cell, "clear", [cell.candidates, []])
             cell.clear()
+            self.currMove = move
     
     def removeCandidate(self, row: int, col: int, number: int) -> None:
         """Removes candidate from Cell
@@ -85,9 +103,13 @@ class Sudoku:
             number (int): candidate
         """
         cell: Cell = self.getCell(row,col)
-        if not cell.fixed and not cell.filled:
+        if not cell.fixed and not cell.filled and number in cell.candidates:
+            
+            move = Move(self.currMove)
+            move.addChange(cell, "removeCandidate", number)
             cell.removeCandidate(number)
-    
+            self.currMove = move
+                
     def addCandidate(self, row: int, col: int, number: int) -> None:
         """Adds a candidate to Cell
 
@@ -97,8 +119,11 @@ class Sudoku:
             number (int): candidate
         """
         cell: Cell = self.getCell(row,col)
-        if not cell.fixed and not cell.filled:
+        if not cell.fixed and not cell.filled and number not in cell.candidates:
+            move = Move(self.currMove)
+            move.addChange(cell, "addCandidate", number)
             cell.addCandidate(number)
+            self.currMove = move
     
     def getRow(self, row: int) -> List[Cell]:
         """Gets row of Cells
@@ -221,26 +246,129 @@ class Sudoku:
                     return False
         return True
         
+    def undo(self) -> None:
+        """Undos the last move made
+        """
+        
+        if self.currMove == self.startMove:
+            return
+        
+        move = self.currMove
+        self.currMove = self.currMove.prev
+        
+        for change in move.changes:
+            cell: Cell = change[0]
+            changeType = change[1]
+            number = change[2]
+            if changeType == "fill":
+                # Change cell number back to prev and fix the filled bool
+                cell.number = number[0]
+                if number[0] == 0:
+                    cell.filled = False
+            elif changeType == "addCandidate":
+                # Remove candidate
+                cell.removeCandidate(number)
+            elif changeType == "removeCandidate":
+                # Add candidate
+                cell.addCandidate(number)
+            elif changeType == "empty":
+                # Fill cell
+                cell.fill(number[0])
+            elif changeType == "clear":
+                # Set candidates
+                cell.candidates = number[0]
+    
+    def redo(self) -> None:
+        """Redos the last undone move
+        
+        Undos the undo
+        """
+        
+        if self.currMove.next == None:
+            return
+        
+        move = self.currMove.next
+        self.currMove = self.currMove.next
+
+        for change in move.changes:
+            cell: Cell = change[0]
+            changeType = change[1]
+            number = change[2]
+            if changeType == "fill":
+                # Change cell number back to new and fix the filled bool
+                cell.number = number[1]
+                cell.filled = True
+            elif changeType == "addCandidate":
+                # Add candidate
+                cell.addCandidate(number)
+            elif changeType == "removeCandidate":
+                # Remove candidate
+                cell.removeCandidate(number)
+            elif changeType == "empty":
+                # Empty cell
+                cell.empty()
+            elif changeType == "clear":
+                # Clear cell
+                cell.clear()
+    
+    def printMoves(self):
+        move = self.startMove
+        while move.next != None:
+            move = move.next
+            move.show() 
+            print()
     
 def main() -> None:
     date = "puzzles/8-22-24/"
     boards = ["easy", "med", "hard"]
     sols = ["easysol", "medsol", "hardsol"]
-    for i in range(len(boards)):
-        board = Sudoku(date+boards[i])
-        print(date+boards[i])
-        board.show()
-        print(board.isValid())
-        sol = Sudoku(date+sols[i])
-        print(date+sols[i])
-        sol.show()
-        print(sol.isValid())
-        
-    invalid = Sudoku()
-    invalid.fill(1,1,1)
-    invalid.fill(1,2,1)
-    print(invalid.isValid())
     
+    sudoku = Sudoku(date+boards[0])
+    sudoku.show()
+    
+    # Make sure no moves are made when no changes made
+    sudoku.removeCandidate(5,6,1)
+    sudoku.removeCandidate(1,1,5)
+    sudoku.addCandidate(6,5,9)
+    sudoku.clear(1,1)
+    sudoku.empty(1,1)
+    
+    # Make sure fill works with Move
+    sudoku.addCandidate(4,6,1)
+    sudoku.addCandidate(4,6,2)
+    sudoku.addCandidate(4,6,1)
+    sudoku.removeCandidate(4,6,1)
+    sudoku.empty(4,6)
+    sudoku.clear(4,6)
+    sudoku.addCandidate(4,6,1)
+    sudoku.fill(5,6,1)
+    
+    print("Regular")
+    sudoku.show()       
+    sudoku.printMoves()
+    print(sudoku.getCell(4,6).candidates)
+    
+     
+    print("Undo")   
+    sudoku.undo()
+    sudoku.show()
+    sudoku.printMoves()
+    print(sudoku.getCell(4,6).candidates)
+    
+    print("Redo")
+    sudoku.redo()
+    sudoku.show()
+    sudoku.printMoves()
+    print(sudoku.getCell(4,6).candidates)
+
+    print("Break")
+    sudoku.undo()
+    sudoku.addCandidate(5,6,1)
+    sudoku.show()
+    sudoku.printMoves()
+    print(sudoku.getCell(4,6).candidates)
+    print(sudoku.getCell(5,6).candidates)
+
     
     
 if __name__ == "__main__":
