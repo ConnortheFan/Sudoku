a6aa4aa4522ae154516e1c55c0f196eb285e05bc
Connor Fan <ConnortheFan@gmail.com>
2024-09-29 18:05:36 -0700

    Implemented intersect

--- Solver.py
+++ Solver.py
@@ -141,53 +141,179 @@ class Solver(Sudoku):
                                 self.currMove.setDesc(f"Twins at ({twin1.row}, {twin1.col}) ({twin2.row}, {twin2.col})")
                                 return True
         return False 
+    
+    def intersect(self) -> bool:
+        """Finds intersect between row/col and box candidates and adjusts accordingly
+
+        Returns:
+            bool: If any changes were made
+        """
+        # Box eliminates Row/Col
+        for i in range(1,10):
+            boxCells = [cell for cell in self.getBox(i) if cell.filled is False]
+            boxCandidates = list(set([candidate for cell in boxCells for candidate in cell.candidates]))
+            for boxCandidate in boxCandidates:
+                possibleIntersectCells = [boxCell for boxCell in boxCells if boxCandidate in boxCell.candidates]
+                if len(possibleIntersectCells) > 3 or len(possibleIntersectCells) < 1:
+                    continue
+                # Check Row
+                row = possibleIntersectCells[0].row
+                sameRow = True
+                for possibleIntersectCell in possibleIntersectCells:
+                    if row != possibleIntersectCell.row:
+                        sameRow = False
+                if sameRow:
+                    # Check for changes
+                    otherRowCells = [cell for cell in self.getRow(row) if cell not in possibleIntersectCells]
+                    for otherRowCell in otherRowCells:
+                        if boxCandidate in otherRowCell.candidates:
+                            self.removeCandidate(otherRowCells, boxCandidate)
+                            cellCoords = []
+                            for possibleIntersectCell in possibleIntersectCells:
+                                cellCoords.append((possibleIntersectCell.row, possibleIntersectCell.col))
+                            self.currMove.setDesc(f"Box {i} intersect row {row} on candidate {boxCandidate} in cells {cellCoords}")
+                            return True
+                # Check Col
+                col = possibleIntersectCells[0].col
+                sameCol = True
+                for possibleIntersectCell in possibleIntersectCells:
+                    if col != possibleIntersectCell.col:
+                        sameCol = False
+                if sameCol:
+                    # Check for changes
+                    otherColCells = [cell for cell in self.getCol(col) if cell not in possibleIntersectCells]
+                    for otherColCell in otherColCells:
+                        if boxCandidate in otherColCell.candidates:
+                            self.removeCandidate(otherColCells, boxCandidate)
+                            cellCoords = []
+                            for possibleIntersectCell in possibleIntersectCells:
+                                cellCoords.append((possibleIntersectCell.row, possibleIntersectCell.col))
+                            self.currMove.setDesc(f"Box {i} intersect col {col} on candidate {boxCandidate} in cells {cellCoords}")
+                            return True
         
+        # Row eliminates Box
+        for i in range(1, 10):
+            rowCells = [cell for cell in self.getRow(i) if cell.filled is False]
+            rowCandidates = list(set([candidate for cell in rowCells for candidate in cell.candidates]))
+            for rowCandidate in rowCandidates:
+                possibleIntersectCells = [rowCell for rowCell in rowCells if rowCandidate in rowCell.candidates]
+                if len(possibleIntersectCells) > 3 or len(possibleIntersectCells) < 1:
+                    continue
+                # Check Box
+                box = possibleIntersectCells[0].box
+                sameBox = True
+                for possibleIntersectCell in possibleIntersectCells:
+                    if box != possibleIntersectCell.box:
+                        sameBox = False
+                if sameBox:
+                    # Check for changes
+                    otherBoxCells = [cell for cell in self.getBox(box) if cell not in possibleIntersectCells]
+                    for otherBoxCell in otherBoxCells:
+                        if rowCandidate in otherBoxCell.candidates:
+                            self.removeCandidate(otherBoxCells, rowCandidate)
+                            cellCoords = []
+                            for possibleIntersectCell in possibleIntersectCells:
+                                cellCoords.append((possibleIntersectCell.row, possibleIntersectCell.col))
+                            self.currMove.setDesc(f"Row {i} intersect box {box} on candidate {rowCandidate} in cells {cellCoords}")
+                            return True
+        
+        # Col eliminates Box
+        for i in range(1, 10):
+            colCells = [cell for cell in self.getCol(i) if cell.filled is False]
+            colCandidates = list(set([candidate for cell in colCells for candidate in cell.candidates]))
+            for colCandidate in colCandidates:
+                possibleIntersectCells = [colCell for colCell in colCells if colCandidate in colCell.candidates]
+                if len(possibleIntersectCells) > 3 or len(possibleIntersectCells) < 1:
+                    continue
+                # Check Box
+                box = possibleIntersectCells[0].box
+                sameBox = True
+                for possibleIntersectCell in possibleIntersectCells:
+                    if box != possibleIntersectCell.box:
+                        sameBox = False
+                if sameBox:
+                    # Check for changes
+                    otherBoxCells = [cell for cell in self.getBox(box) if cell not in possibleIntersectCells]
+                    for otherBoxCell in otherBoxCells:
+                        if colCandidate in otherBoxCell.candidates:
+                            self.removeCandidate(otherBoxCells, colCandidate)
+                            cellCoords = []
+                            for possibleIntersectCell in possibleIntersectCells:
+                                cellCoords.append((possibleIntersectCell.row, possibleIntersectCell.col))
+                            self.currMove.setDesc(f"Col {i} intersect box {box} on candidate {rowCandidate} in cells {cellCoords}")
+                            return True
+        return False
+
+def test():
+    board = "puzzles/9-27-24/med"
+    s = Sudoku(board)
+    solver = Solver(s)
+    change = True
+    while change:
+        change = solver.nakedSingle()
+        if not change:
+            change = solver.clothedSingle()
+        if not change:
+            change = solver.twins()
+        if not change:
+            change = solver.intersect()
+    
+    
+    solver.printMoves()
+    s.show()
+    solver.show()
+    return
+
 def main() -> None:
     dates = ["8-21-24/", "8-22-24/", "9-22-24/"]
     boards = ["easy", "med", "hard"]
     
-    for date in dates:
-        for board in boards:
-            print(date+board)   
-            s = Sudoku("puzzles/" + date + board)
-            sol = Sudoku("puzzles/" + date + board + "sol")
-            solver = Solver(s)
+    # for date in dates:
+    #     for board in boards:
+    #         print(date+board)   
+    #         s = Sudoku("puzzles/" + date + board)
+    #         sol = Sudoku("puzzles/" + date + board + "sol")
+    #         solver = Solver(s)
             
-            change = True
-            while change:    
-                change = solver.twins()
+    #         change = True
+    #         while change:    
+    #             change = solver.twins()
             
                 
-            solver.printMoves()
-            print("Original")
-            s.show()
-            print("Solver Result")
-            solver.show()
-            print("Solution")
-            sol.show()
+    #         solver.printMoves()
+    #         print("Original")
+    #         s.show()
+    #         print("Solver Result")
+    #         solver.show()
+    #         print("Solution")
+    #         sol.show()
     
     
-    # name = "puzzles/9-22-24/med"
-    # s = Sudoku(name)
-    # solver = Solver(s)    
-    # solver.show()
-    # change = True
-    # while change:    
-    #     change = solver.nakedSingle()
-    # solver.printMoves()
-    # solver.show()
-    # solution = ""
-    # for row in solver.board:
-    #     for j, cell in enumerate(row):
-    #         solution += f'{cell.number}'
-    #         if j == 8:
-    #             solution += f'\n'
-    #         else:
-    #             solution += ' '
-    # print(solution)
-    # f = open(name+'sol', 'w')
-    # f.write(solution)
-    # f.close()
+    name = "puzzles/9-27-24/med"
+    s = Sudoku(name)
+    solver = Solver(s)    
+    solver.show()
+    change = True
+    while change:    
+        change = solver.nakedSingle()
+        if not change:
+            change = solver.clothedSingle()
+        if not change:
+            change = solver.twins()
+    solver.printMoves()
+    solver.show()
+    solution = ""
+    for row in solver.board:
+        for j, cell in enumerate(row):
+            solution += f'{cell.number}'
+            if j == 8:
+                solution += f'\n'
+            else:
+                solution += ' '
+    print(solution)
+    f = open(name+'sol', 'w')
+    f.write(solution)
+    f.close()
     
 if __name__ == "__main__":
-    main()
+    test()
